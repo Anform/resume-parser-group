7e85017fe8a9f753425e841dc000361883c9cc6c
Youkwhan <37788922+Youkwhan@users.noreply.github.com>
2022-08-11 22:00:54 -0400

    firebase error fix

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -1,6 +1,5 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "@firebase/firestore"
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -22,4 +21,3 @@ const app = initializeApp(firebaseConfig);
 
 //All the connections needed related to firebase, now we need to start using them
 export const db = getFirestore(app);
-export const analytics = getAnalytics(app);
