8754cce40dbad3602c4ad321ea79a532d2afc2fd
Agustin Escoda <32514472+Anform@users.noreply.github.com>
2022-08-12 22:21:46 -0400

    Fixed FileUploadPage.js
    Fixed upload to firebase and searching for languages

--- src/components/FileUploadPage.js
+++ src/components/FileUploadPage.js
@@ -1,32 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import langFile from "../test-files/languages.txt"
 import educationFile from "../test-files/education.txt"
 import {collection, addDoc} from "firebase/firestore"
 import {db} from "../firebase-config"
-
 function FileUploadPage() {
+
+	const [lanArray, setLanArray] = useState(["Python","Java","JavaScript","C#","C","C++", "PHP", "R", "TypeScript","Objective-C","Swift","Matlab","Kotlin","Go", 
+	"Rust", "Ruby","VBA","Ada","Scala", "Dart", "Abap","Lua", "Groovy", "Perl", "Julia", "Cobol", "Haskell", "Delphi", "Pascal"])
+
+	const [diplomaArr, setDiplomaArr] = useState(["diploma", "associate","bachelor", "master", "doctorate"])
 
 	const getName = (textFile) => {
 		return textFile.split(" ").slice(0, 2).join(" ");
 	};
 
 	const getLanguages=(textFile)=>{
-		return getWords(langFile, textFile)
+		return getWords(lanArray, textFile)
 		
 	}
 
 	const getEducation=(textFile) =>{
-		return getWords(educationFile, textFile)
+		return getWords(diplomaArr, textFile)
 	}
 
-	const getWords = (sourceFile, resumeFile) => {
+	const getWords = (array, resumeFile) => {
 		//First we need to get sourceFile 
-		fetch(sourceFile)
-			.then((response) => response.text())
-			.then((data) => {
 				// Match data with resumeFile
-				const sourceFileWords = data.split("\n"); //split source file by spaces
-				console.log(sourceFileWords)
+				//console.log("Source File; ",array)
 				
 				/*//is this an array? let's find out.
 				const text = sourceFileWords.toString();
@@ -34,8 +34,8 @@ function FileUploadPage() {
 
 				var wordsList = []; //words present in the resume will be stored here.
 				//for each word in the sourceFile, check if it's in the resumeFile
-				for(var i=0; i<sourceFileWords.length; i++){ //iterate through all words of source file
-					var currentWord = sourceFileWords[i]; //current word being looked at
+				for(var i=0; i<array.length; i++){ //iterate through all words of source file
+					var currentWord = array[i]; //current word being looked at
 					//console.log("current word: ", currentWord);
 					//what does the resume file look like?
 					//console.log("resume: ", resumeFile);
@@ -44,19 +44,19 @@ function FileUploadPage() {
 					}
 				}
 				//print out the words list for inspection
-				console.log(wordsList);
+				//console.log(wordsList);
 				//turn it into a string for export
 				wordsList = wordsList.toString(); //use toString() to turn array into string. elements seperated by commas.
     			//ex: ["apples", "oranges", "bananas"] -> "apples,oranges,bananas"
 				//print out the words list for inspection
-				console.log(wordsList);
-    			return wordsList;
+				//console.log(wordsList);
+				return wordsList
 
-			});
 	}
 
 	const saveChange = async (name, languages, education) => {
 		//addDoc() cloud firestore auto generates an ID 
+
 		await addDoc(collection(db, "resume"), {
 			//our data we are storing OR fields
 			name: name,
@@ -69,6 +69,7 @@ function FileUploadPage() {
 			.catch((error) => {
 				console.log(error);
 			});
+		window.location.reload()
 	};
 
 	const handleFileChange = (e) => {
@@ -77,20 +78,23 @@ function FileUploadPage() {
 		fileReader.onload = async (e) => {
 			const text = e.target.result;
 			// console.log(text);
-			var textFile = text.replace(/\n/g, " ");
+			var textFile = ""
+			textFile = text.replace(/\n/g, " ");
 			//console.log(textFile);
 
 			const name = getName(textFile);
 			//console.log(name);
-			
-			const languages = getLanguages(textFile)
-			//console.log(languages)
 
-			const education = getEducation(textFile) //education
-			//console.log(education);
+			var languages = getLanguages(textFile)
+			//console.log(languages)
+			
+			var education = getEducation(textFile)
+			//console.log(education)
 			
 			//upload to fb
 			saveChange(name, languages, education)
+
+			
 		};
 
 		fileReader.readAsText(e.target.files[0]);
